f703b90f0adf9ee1b8056cea373e7b5244db69ed
Roman <destabiliseus@gmail.com>
2022-12-11 22:48:10 +0800

    chore: update expiresIn value

--- backend/src/authentication/authentication.module.ts
+++ backend/src/authentication/authentication.module.ts
@@ -13,7 +13,7 @@ import { LocalStrategy } from './local.strategy';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '30m' },
     }),
   ],
   controllers: [AuthenticationController],
